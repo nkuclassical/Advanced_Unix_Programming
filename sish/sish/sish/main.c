a8640fa24139d370cbc76297ffe7cbbf2f7398c3
Yanqiao <yzhan14@stevens.edu>
2015-12-10 09:10:27 -0500

    test

---
+++ sish/sish/sish/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  sish
+//
+//  Created by Yanqiao Zhan on 12/10/15.
+//  Copyright © 2015 Yanqiao Zhan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    return 0;
+}
